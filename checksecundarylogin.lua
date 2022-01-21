53c9032d2bb7aa6764a0543963fac60b4baa0ff9
happycarrot1234 <94752417+happycarrot1234@users.noreply.github.com>
2022-01-21 17:25:58 -0500

    Update checksecundarylogin.lua

--- checksecundarylogin.lua
+++ checksecundarylogin.lua
@@ -2,6 +2,7 @@ local username_true = {}
 local username = globals.get_username()
 username_true['courvixEZ'] = true
 username_true['widest'] = true
+username_true['Bell'] = true
 
 local function sendtrue()
     if username_true[username] == true then
